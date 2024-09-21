441c1cb772b1069a98365cfc6c9b54c1f3c379de
Maryuha Daria <mariukha.daria@student.sumdu.edu.ua>
2024-09-21 18:43:09 +0300

    Update num2.py

--- num2.py
+++ num2.py
@@ -1,14 +1,19 @@
-n = 7  #Задаємо розмірність матриці
+def replace_negatives():
+    #Введення списку чисел з клавіатури
+    A = list(map(int, input('Enter a list of integers separated by spaces: ').split()))
 
-#Створюємо порожній список 
-a = [[0] * n for _ in range(n)]
+    #Перевіряємо наявність від'ємних чисел
+    has_negatives = any(num < 0 for num in A)
+    if not has_negatives:
+        print("No negative numbers found.")
 
-#Заповнення матриці за умовою
-for j in range(n):  
-    for i in range(n):  
-        if i >= j:  
-            a[j][i] = (n - i + j)  
+    #Замінюємо всі від'ємні елементи на 0
+    result = [0 if num < 0 else num for num in A]
 
-#Виводимо кожен рядок матриці
-for r in a:  
-    print(*r)  #Виводимо елементи рядка через пробіл
+    #Виводимо список
+    print("Modified list:", result)
+
+    return result
+
+replace_negatives()
+
