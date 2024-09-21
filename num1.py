60803fa276505fe95a8248d1f29f23b28fd29fa6
Maryuha Daria <mariukha.daria@student.sumdu.edu.ua>
2024-09-21 18:42:47 +0300

    Update num1.py

--- num1.py
+++ num1.py
@@ -1,23 +1,20 @@
-#Введення кількості елементів у масиві
-N = int(input("N = "))
+def sortlist():
+    #Введення рядка з елементами
+    A = input('Enter a list of numbers and letters separated by spaces: ').split()
+    #Окремо для цифр і літер
+    digits = []
+    letters = []
+    #Перебираємо елементи і розподіляємо їх між цифрами та літерами
+    for elem in A:
+        if elem.isdigit():  #Якщо елемент цифра
+            digits.append(int(elem))
+        elif elem.isalpha():  #Якщо елемент літера
+            letters.append(elem)
 
-#Введення елементів масиву
-print(f"Enter {N} array elements:")
-arr = [float(input()) for _ in range(N)]
-#Виведення заданого масиву
-print("Original array: ", arr)
+    #Виведення результату
+    print("Digits:", digits)
+    print("Letters:", letters)
 
-# Ініціалізація змінної для мінімального від'ємного елемента
-min = None
+    return digits, letters
+sortlist()
 
-#Знаходження мінімального елемента масиву
-for num in arr:
-  if num < 0: #перевірка на від'ємність
-      if min is None or num < min: #перевірка для збереження мінімуму
-          min = num
-
-#Виведення результату
-if min is not None:
-  print("Minimum negative element: ", min)
-else:
-  print("No negative elements in the array.")
